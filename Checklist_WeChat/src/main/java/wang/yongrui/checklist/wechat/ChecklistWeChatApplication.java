4df7d45634f31cc3f3e5bb32fabaee3ced4b4f9c
RyanWang1982 <w_y_r@live.cn>
2017-06-15 07:38:31 +0800

    Initial Version

---
+++ Checklist_WeChat/src/main/java/wang/yongrui/checklist/wechat/ChecklistWeChatApplication.java
@@ -0,0 +1,12 @@
+package wang.yongrui.checklist.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecklistWeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChecklistWeChatApplication.class, args);
+	}
+}
---
+++ Checklist_WeChat/src/main/java/wang/yongrui/checklist/wechat/ServletInitializer.java
@@ -0,0 +1,13 @@
+package wang.yongrui.checklist.wechat;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ChecklistWeChatApplication.class);
+	}
+
+}

e5ee8bd2666263e6ecac1f05e13b80de8e0e7062
RyanWang1982 <w_y_r@live.cn>
2017-07-05 00:24:06 +0800

    Remove redundant object.

--- Checklist_WeChat/src/main/java/wang/yongrui/checklist/wechat/ChecklistWeChatApplication.java
+++ Checklist_WeChat/src/main/java/wang/yongrui/checklist/wechat/ChecklistWeChatApplication.java
@@ -2,11 +2,18 @@ package wang.yongrui.checklist.wechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class ChecklistWeChatApplication {
+public class ChecklistWeChatApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ChecklistWeChatApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChecklistWeChatApplication.class, args);
